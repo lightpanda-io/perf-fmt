394b6ec5ca131a23a25da404abe88f8a36bae7fa
Pierre Tachoire <pierre@lightpanda.io>
2023-12-05 16:03:04 +0100

    add commit hash argument

--- append.go
+++ append.go
@@ -3,8 +3,15 @@ package main
 import (
 	"context"
 	"io"
+
+	"github.com/browsercore/perf-fmt/git"
 )
 
 type Append interface {
-	Append(ctx context.Context, out io.Writer, all io.Reader, one io.Reader) error
+	Append(ctx context.Context,
+		hash git.CommitHash,
+		out io.Writer,
+		all io.Reader,
+		one io.Reader,
+	) error
 }
